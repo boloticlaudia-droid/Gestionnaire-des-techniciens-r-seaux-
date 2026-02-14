b61b5906ec5d6dbf3373dc5ac7fcbdb252c65e58
Kubanza Wa Kubanza Elvira <elviraflorentk@gmail.com>
2026-02-14 15:34:33 +0100

    Création de la table des techniciens

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ connexion=sqlite3.connect("Techniciens.db")
 
 curseur=connexion.cursor()
 
+curseur.execute("""
+CREATE TABLE IF NOT EXISTS techniciens (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+    age INTEGER,
+    specialite TEXT,
+    niveau TEXT,
+    experience INTEGER,
+    domaine TEXT,
+    grade TEXT,
+    telephone TEXT NOT NULL,
+    email TEXT,
+    ville TEXT
+)
+""")
+
 
 
 
