d4d15a4cc03955e9bdcbc747c7feeb872ba2138e
Kubanza Wa Kubanza Elvira <elviraflorentk@gmail.com>
2026-02-15 20:04:06 +0100

    Création label7 (Expérience)

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ label1=tk.Label(frame1,text="Nom",font="Anton 11",bg="white")
 label1.place(x=3,y=7)
 
 zone1=tk.Entry(frame1,bg="white")
-zone1.place(x=78,y=12)
+zone1.place(x=120,y=12)
 
 
 label2=tk.Label(frame1,text="Prénom",font="Anton 11",bg="white")
@@ -49,33 +49,36 @@ label2.place(x=3,y=43)
 #Prof ! Ceci je le note juste pour signaler certaines modifications au code, plus j'avance et je rentre parfois en arrière pour modifier certains détails comme la couleur, ou la position de la zone1 par exemple
 
 zone2=tk.Entry(frame1,bg="white")
-zone2.place(x=78,y=46)
+zone2.place(x=120,y=46)
 
 label3=tk.Label(frame1,text="Age",font="Anton 11",bg="white")
 label3.place(x=3,y=77)
 
 zone3=tk.Entry(frame1,bg="white")
-zone3.place(x=78,y=82)
+zone3.place(x=120,y=82)
 
 label4=tk.Label(frame1,text="Sexe",font="Anton 11",bg="white")
 label4.place(x=3,y=109)
 
 combo=ttk.Combobox(frame1,values=["Homme","Femme"])
 combo.set("Sélectionner")
-combo.place(x=70,y=110)
+combo.place(x=110,y=110)
 
 
 label5=tk.Label(frame1,text="Spécialité",font="Anton 11",bg="white")
 label5.place(x=3,y=148)
 
 zone5=tk.Entry(frame1,bg="white")
-zone5.place(x=78,y=151)
+zone5.place(x=120,y=151)
 
 label6=tk.Label(frame1,text="Niveau d'étude",font="Anton 11",bg="white")
 label6.place(x=3,y=182)
 
 zone6=tk.Entry(frame1,bg="white")
 zone6.place(x=120,y=184)
+
+label7=tk.Label(frame1,text="Expérience",font="Anton 11",bg="white")
+label7.place(x=3,y=213)
 
 
 app.mainloop()
