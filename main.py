3d655b5a070e08712dbf91982afa940b0bb292fd
Kubanza Wa Kubanza Elvira <elviraflorentk@gmail.com>
2026-02-15 11:48:34 +0100

    Création du premier label (Nom)

--- main.py
+++ main.py
@@ -31,8 +31,11 @@ app=tk.Tk()
 app.geometry("800x600")
 app.title("Gestionnaire des techniciens")
 
-frame1=tk.LabelFrame(app,text="Informations Techniciens",width=500,height=380,bg="white",fg="black",font="Anton 12")
+frame1=tk.LabelFrame(app,text="Informations Techniciens",width=500,height=380,bg="white",fg="black",font="Anton 10")
 frame1.place(x=10,y=15)
+
+label1=tk.Label(frame1,text="Nom",font="Anton 18",bg="white")
+label1.place(x=3,y=7)
 
 
 
