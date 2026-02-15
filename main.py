0b77ac5695213380fe61a9d673dbea06d8e86d88
Kubanza Wa Kubanza Elvira <elviraflorentk@gmail.com>
2026-02-15 15:52:21 +0100

    Je fais juste des petites modifications pour embellir le tout

--- main.py
+++ main.py
@@ -31,28 +31,28 @@ app=tk.Tk()
 app.geometry("800x600")
 app.title("Gestionnaire des techniciens")
 
-frame1=tk.LabelFrame(app,text="Informations Techniciens",width=500,height=380,bg="gray",fg="black",font="Anton 10")
+frame1=tk.LabelFrame(app,text="Informations Techniciens",width=500,height=380,bg="white",fg="black",font="Anton 10")
 frame1.place(x=10,y=15)
 
-label1=tk.Label(frame1,text="Nom",font="Anton 18",bg="gray")
+label1=tk.Label(frame1,text="Nom",font="Anton 11",bg="white")
 label1.place(x=3,y=7)
 
 zone1=tk.Entry(frame1,bg="white")
-zone1.place(x=110,y=14)
+zone1.place(x=70,y=12)
 
 
-label2=tk.Label(frame1,text="Prénom",font="Anton 18",bg="gray")
+label2=tk.Label(frame1,text="Prénom",font="Anton 11",bg="white")
 label2.place(x=3,y=43)
 
 #Prof ! Ceci je le note juste pour signaler certaines modifications au code, plus j'avance et je rentre parfois en arrière pour modifier certains détails comme la couleur, ou la position de la zone1 par exemple
 
 zone2=tk.Entry(frame1,bg="white")
-zone2.place(x=110,y=50)
+zone2.place(x=70,y=46)
 
-label3=tk.Label(frame1,text="Age",font="Anton 18",bg="gray")
+label3=tk.Label(frame1,text="Age",font="Anton 11",bg="white")
 label3.place(x=3,y=77)
 
 zone3=tk.Entry(frame1,bg="white")
-zone3.place(x=110,y=85)
+zone3.place(x=70,y=82)
 
 app.mainloop()
